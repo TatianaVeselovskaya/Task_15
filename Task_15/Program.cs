682d7bda5eef73074d0a550980af1bdbd46363a9
Татьяна <veselovskayt@mail.ru>
2021-12-26 17:52:53 +0700

    Добавьте файлы проекта.

---
+++ Task_15/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Арифметическая прогрессия");
+            ArithProgression arith = new ArithProgression();
+            arith.setStart(6);
+            arith.setStep(3);
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            Console.WriteLine(arith.getNext());
+            arith.reset();
+            Console.WriteLine(arith.getNext()); 
+            Console.WriteLine(arith.getNext());
+
+            Console.WriteLine();
+            Console.WriteLine("Геометрическая прогрессия");
+            GeomProgression geom = new GeomProgression();
+            geom.setStart(5);
+            geom.setStep(2);
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            geom.reset();
+            Console.WriteLine(geom.getNext());
+            Console.WriteLine(geom.getNext());
+            Console.ReadKey();
+        }
+    }
+}
